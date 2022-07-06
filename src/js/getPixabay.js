aae6b934b692b48da4cca2939f58d488ba6a9bd3
VladIT26 <vlad.ua26@gmail.com>
2022-07-06 16:42:13 +0300

    js

--- src/js/getPixabay.js
+++ src/js/getPixabay.js
@@ -1,21 +1,43 @@
 import axios from "axios";
-
 const APIKEY = '28443964-68fadfc1b5ff1bceef8020642';
 
-const options = new UrlSearchOptions({
-    q: 'cat',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-});
-
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-axios.defaults.headers.common['Authorization'] = APIKEY;
+// axios.defaults.headers.common['key'] = APIKEY;
 
 export class GetPixabayApi {
-    constructor(params) { }
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
 
-    fetchImages() {
-    axios.get('https://pixabay.com/api/', options)
+    async fetchImages() {
+        const options = new URLSearchParams({
+            key: APIKEY,
+            q: this.searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            page: this.page,
+            per_page: 40,
+        })
+        const { data } = await axios.get(`?${options}`);
+        this.incrementPage();
+        return data;
+    }
+
+    get searchQuery1() {
+        return this.searchQuery;
+    }
+
+    set searchQuery1(newSearchQuery) {
+        this.searchQuery = newSearchQuery
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,111 @@
 import './../css/styles.css';
-import { Axios } from 'axios';
 import { Notify } from 'notiflix';
-import GetPixabayApi from './getPixabay';
+import { GetPixabayApi } from './getPixabay';
+import SimpleLightbox from "simplelightbox";
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+const formRef = document.querySelector('.search-form');
+const loadMoreButton = document.querySelector('.load-more');
+
+formRef.addEventListener('submit', onFormSubmit);
+loadMoreButton.addEventListener('click', onLoadMoreButtonClick);
 
 const getPixabayApi = new GetPixabayApi();
-GetPixabayApi.fetchImages();
+showLoading(false);
+
+
+function makeGallaryMarkup(searchedImages) {
+    return searchedImages.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+    }) => `<div class="photo-card"><a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`).join('')
+}
+
+function renderGallery(searchedImages) {
+    galleryRef.insertAdjacentHTML('beforeend', makeGallaryMarkup(searchedImages))
+}
+
+async function onFormSubmit(event) {
+    event.preventDefault();
+    showLoading(true);
+    cleanGallery();
+    getPixabayApi.resetPage();
+    const request = event.target.elements.searchQuery.value.trim();
+    if (!request) { return Notify.info("Input text to search") };
+    getPixabayApi.searchQuery1 = request;
+    try {
+        const { hits, totalHits } = await getPixabayApi.fetchImages();
+      
+        if (!totalHits) {
+             showLoading(false);
+            return Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+        }
+    Notify.success(`Hooray! We found ${totalHits} images.`)
+        renderGallery(hits);
+        lightbox.refresh();
+    } catch (error) {
+        console.log(error.message)
+    }
+    event.target.reset();
+}
+
+async function onLoadMoreButtonClick() {
+    showLoading(true)
+    try {
+        const { hits, totalHits } = await getPixabayApi.fetchImages();
+        if (galleryRef.childElementCount >= totalHits) {
+            showLoading(false);
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+        renderGallery(hits); 
+        lightbox.refresh();
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+}); 
+    } catch (error) {
+     console.log(error.message)
+    }
+}
+
+function cleanGallery() {
+    galleryRef.innerHTML = '';
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 300, 
+});
+
+
+function showLoading(status) {
+  loadMoreButton.style.display = status ? 'block' : 'none';
+}
+
+
